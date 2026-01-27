6af14632700a4972c2126d87eb7fe675affb3fd3
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-01-26 19:18:44 -0700

    Jokes page completed

--- src/apis/http.jsx
+++ src/apis/http.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'http://localhost:3000/api/jokes',
+  baseURL: 'http://localhost:3000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
--- src/pages/Jokes.jsx
+++ src/pages/Jokes.jsx
@@ -1,8 +1,38 @@
+import { getJokes } from '@/apis/jokes'
+import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+
 const Jokes = () => {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    const loadJokes = async () => {
+      try {
+        const res = await getJokes()
+        setJokes(res)
+      } catch (error) {
+        console.log(error)
+        throw new Error('fetch channel error')
+      }
+    }
+    loadJokes()
+  }, [])
+
   return (
     <>
       <h2>Jokes</h2>
-      <div>this is Jokes page</div>
+      <ul>
+        {jokes.map(item => (
+          <li key={item.joke_id}>
+            {item.content} --{' '}
+            <span>
+              {dayjs(item.joke_created_at).format('MMM D, YYYY HH:mm')}
+            </span>{' '}
+            --
+            <span>like count: {item.like_count}</span>
+          </li>
+        ))}
+      </ul>
     </>
   )
 }
