8231fa5674fc63c56bd7fb63fd593183a1da98df
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-01-27 22:41:12 -0700

    Jokes page finished

--- src/pages/Jokes.jsx
+++ src/pages/Jokes.jsx
@@ -2,6 +2,11 @@ import { getJokes } from '@/apis/jokes'
 import { useEffect, useState } from 'react'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import {
+  DescriptionDetails,
+  DescriptionList,
+  DescriptionTerm,
+} from '@/components/description-list'
 
 dayjs.extend(relativeTime)
 
@@ -21,17 +26,30 @@ const Jokes = () => {
   }, [])
 
   return (
-    <>
-      <ul>
-        {jokes.map(item => (
-          <li key={item.joke_id}>
-            {item.content} --{' '}
-            <span>{dayjs(item.joke_created_at).fromNow()}</span> --
-            <span>likes: {item.like_count}</span>
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="space-y-8">
+      {jokes.map(item => (
+        <div className="bg-white/05 backdrop-blur-3xl rounded-2xl shadow-2xl border border-white/30 p-4">
+          <DescriptionList>
+            <DescriptionTerm>
+              <span className="text-black/80">Joke Content</span>
+            </DescriptionTerm>
+            <DescriptionDetails>{item.content}</DescriptionDetails>
+
+            <DescriptionTerm>
+              <span className="text-black/80">Created At</span>
+            </DescriptionTerm>
+            <DescriptionDetails>
+              {dayjs(item.joke_created_at).fromNow()}
+            </DescriptionDetails>
+
+            <DescriptionTerm>
+              <span className="text-black/80">Likes</span>
+            </DescriptionTerm>
+            <DescriptionDetails>{item.like_count}</DescriptionDetails>
+          </DescriptionList>
+        </div>
+      ))}
+    </div>
   )
 }
 
