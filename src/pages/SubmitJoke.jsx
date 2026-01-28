b0dedb47e45b0c0e6611fc77977621c437bdaf3a
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-01-28 09:51:16 -0700

    improve experience on submitJokes page, set label for, set the way of enter

--- src/pages/SubmitJoke.jsx
+++ src/pages/SubmitJoke.jsx
@@ -9,7 +9,8 @@ import { useNavigate } from 'react-router-dom'
 function SubmitJoke() {
   const [content, setContent] = useState('')
   const navigate = useNavigate()
-  const handleSubmit = () => {
+  const handleSubmit = e => {
+    if (e?.preventDefault) e.preventDefault()
     if (!content) {
       toast.error('Please enter a joke')
       return
@@ -25,20 +26,31 @@ function SubmitJoke() {
   }
 
   return (
-    <Field className="space-y-8">
-      <Label className="!text-4xl">Input your joke</Label>
-      <Description className="!text-white">
-        Let's share your joke with the world!
-      </Description>
-      <Textarea
-        name="content"
-        id="content"
-        onChange={e => setContent(e.target.value)}
-      />
-      <Button onClick={handleSubmit} className="!text-black !bg-amber-100">
-        Submit
-      </Button>
-    </Field>
+    <form onSubmit={handleSubmit}>
+      <Field className="space-y-8">
+        <Label htmlFor="content" className="!text-4xl">
+          Input your joke
+        </Label>
+        <Description className="!text-white">
+          Let's share your joke with the world!
+        </Description>
+        <Textarea
+          name="content"
+          id="content"
+          value={content}
+          onChange={e => setContent(e.target.value)}
+          onKeyDown={e => {
+            if (e.key === 'Enter' && !e.shiftKey) {
+              e.preventDefault()
+              handleSubmit()
+            }
+          }}
+        />
+        <Button type="submit" className="!text-black !bg-amber-100">
+          Submit
+        </Button>
+      </Field>
+    </form>
   )
 }
 
