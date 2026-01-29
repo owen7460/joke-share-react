f8742c14decff3d74303cf678bd208e42b068f53
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-01-28 18:16:43 -0700

    modify layout on weather page

--- src/pages/weather.jsx
+++ src/pages/weather.jsx
@@ -19,28 +19,38 @@ function Weather() {
 
   return (
     <>
-      <div className="flex justify-center items-center gap-24 min-h-full">
-        <div>
-          <h1 className="text-5xl text-white font-bold">{weather.name}</h1>
-          <img
-            className="w-20 mx-auto h-20"
-            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
-            alt={weather.weather[0].description}
-          />
-          <p className="text-white text-2xl">{weather.weather[0].main}</p>
-          <p className="text-white">{weather.weather[0].description}</p>
+      <div className="flex justify-center items-center gap-14 min-h-full">
+        <div className="flex flex-col gap-4">
+          <h1 className="text-9xl text-white font-bold">{weather.name}</h1>
+          <div className="flex items-center gap-4">
+            <img
+              className="w-20 mr-auto h-20"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+              alt={weather.weather[0].description}
+            />
+            <p className="text-white text-2xl">{weather.weather[0].main}</p>
+            <p className="text-white text-2xl font-thin">
+              {weather.weather[0].description}
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-col gap-4">
-          <p className="text-white">Temperature: {weather.main.temp}°C</p>
-          <p className="text-white">
+        <div className="flex flex-col gap-1">
+          <p className="text-white text-2xl">
+            Temperature: {weather.main.temp}°C
+          </p>
+          <p className="text-white text-2xl">
             Temperature max: {weather.main.temp_max}°C
           </p>
-          <p className="text-white">
+          <p className="text-white text-2xl">
             Temperature min: {weather.main.temp_min}°C
           </p>
-          <p className="text-white">Humidity: {weather.main.humidity}%</p>
-          <p className="text-white">Feels like: {weather.main.feels_like}°C</p>
+          <p className="text-white text-2xl">
+            Humidity: {weather.main.humidity}%
+          </p>
+          <p className="text-white text-2xl">
+            Feels like: {weather.main.feels_like}°C
+          </p>
         </div>
 
         <div className="flex flex-col gap-2"></div>
