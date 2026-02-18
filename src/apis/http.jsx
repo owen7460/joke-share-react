68cc32a81d779fef319a956ab81b174a2b3f2717
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-02-17 17:12:15 -0700

    with vite api url

--- src/apis/http.jsx
+++ src/apis/http.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: 'https://api.owenouyang.com/api/',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
