628954228e9844ad5783969803029ba5a64b9b36
Owen <Xiongwen.Ouyang@sait.edu.ca>
2026-04-10 16:04:33 -0600

    add persisit

--- src/stores/jokesStore.jsx
+++ src/stores/jokesStore.jsx
@@ -1,10 +1,19 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
-const useJokesStore = create(set => ({
-  jokes: [],
-  setJokes: jokes => set({ jokes }),
-  removeJoke: id =>
-    set(state => ({ jokes: state.jokes.filter(item => item.joke_id !== id) })),
-}))
-
+const useJokesStore = create(
+  persist(
+    set => ({
+      jokes: [],
+      setJokes: jokes => set({ jokes }),
+      removeJoke: id =>
+        set(state => ({
+          jokes: state.jokes.filter(item => item.joke_id !== id),
+        })),
+    }),
+    {
+      name: 'jokes-storage',
+    }
+  )
+)
 export default useJokesStore
